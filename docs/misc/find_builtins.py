ebdd20f38a78398e88eb12e80437fd158f4d154d
Jiajie Chen <c@jia.je>
2026-07-28 10:35:53 +0800

    Find more builtins

--- docs/misc/find_builtins.py
+++ docs/misc/find_builtins.py
@@ -6,6 +6,19 @@ for obj in [(), [], {}, ""]:
         for attr in dir(c):
             try:
                 if "__builtins__" in getattr(c, attr).__globals__:
-                    print(repr(obj), i, attr, c)
+                    code = f'{repr(obj)}.__class__.__subclasses__()[{i}].{attr}.__globals__["__builtins__"]'
+                    assert "breakpoint" in eval(code)
+                    print(code)
             except:
                 pass
+
+classes = ().__class__.__base__.__subclasses__()
+for i, c in enumerate(classes):
+    for attr in dir(c):
+        try:
+            if "__builtins__" in getattr(c, attr).__globals__:
+                code = f'().__class__.__base__.__subclasses__()[{i}].{attr}.__globals__["__builtins__"]'
+                assert "breakpoint" in eval(code)
+                print(code)
+        except:
+            pass
